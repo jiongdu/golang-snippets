bba576dfcb81d651a4918f44d6ab11b570269fe6
jiongdu <jonnydu.uestc@gmail.com>
2020-06-26 21:22:49 +0800

    stype: mapstructure

--- mapstructure/mapstructure.go
+++ mapstructure/mapstructure.go
@@ -1,25 +1,31 @@
-package mapstructure
+package main
 
 import (
 	"fmt"
+
 	"github.com/mitchellh/mapstructure"
 )
 
 type ChatSetting struct {
-	ConsumeTotal  string  `mapstructure:"consume_total"`
-	FansClubLevel string  `mapstructure:"fans_club_level"`
-	UserLevel     string  `mapstructure:"user_level"`
-	OnlySeeFans   string `mapstructure:"only_see_fans"`
+	ConsumeTotal  string          `mapstructure:"consume_total"`
+	FansClubLevel map[string]bool `mapstructure:"fans_club_level"`
+	UserLevel     string          `mapstructure:"user_level"`
+	OnlySeeFans   string          `mapstructure:"only_see_fans"`
 }
 
 func main() {
+
+	// mapstructure.Decode
+	fansClubLevel := make(map[string]bool)
+	fansClubLevel["300"] = true
 	m := make(map[string]interface{})
 	m["consume_total"] = "300"
-	m["fans_club_level"] = "200"
+	m["fans_club_level"] = fansClubLevel
 	m["user_level"] = "100"
 	m["only_see_fans"] = true
 
 	chatSetting := ChatSetting{}
 	mapstructure.Decode(m, &chatSetting)
-	fmt.Println("value:", chatSetting.ConsumeTotal)
+	fmt.Println("value:", chatSetting.FansClubLevel)
+
 }
